53301c081220f17ece59716450a7fb501beb2d95
Vivek S <demouser@Viveks-MacBook-Pro.local>
2025-07-17 17:08:52 +0530

    login page added

---
+++ b.kt
@@ -0,0 +1,110 @@
+fun isPrime(num: Int): Boolean {
+    if (num < 2) return false
+    for (i in 2..num / 2) {
+        if (num % i == 0) return false
+    }
+    return true
+}
+
+fun main() {
+    println("Enter how many prime numbers to print:")
+    val n = readln().toInt()
+    var count = 0
+    var number = 2
+
+    while (count < n) {
+        if (isPrime(number)) {
+            println(number)
+            count++
+        }
+        number++
+    }
+}
+fun isPrime(num: Int): Boolean {
+    if (num < 2) return false
+    for (i in 2..num / 2) {
+        if (num % i == 0) return false
+    }
+    return true
+}
+
+fun main() {
+    println("Enter how many prime numbers to print:")
+    val n = readln().toInt()
+    var count = 0
+    var number = 2
+
+    while (count < n) {
+        if (isPrime(number)) {
+            println(number)
+            count++
+        }
+        number++
+    }
+}
+fun isPrime(num: Int): Boolean {
+    if (num < 2) return false
+    for (i in 2..num / 2) {
+        if (num % i == 0) return false
+    }
+    return true
+}
+
+fun main() {
+    println("Enter how many prime numbers to print:")
+    val n = readln().toInt()
+    var count = 0
+    var number = 2
+
+    while (count < n) {
+        if (isPrime(number)) {
+            println(number)
+            count++
+        }
+        number++
+    }
+}
+fun isPrime(num: Int): Boolean {
+    if (num < 2) return false
+    for (i in 2..num / 2) {
+        if (num % i == 0) return false
+    }
+    return true
+}
+
+fun main() {
+    println("Enter how many prime numbers to print:")
+    val n = readln().toInt()
+    var count = 0
+    var number = 2
+
+    while (count < n) {
+        if (isPrime(number)) {
+            println(number)
+            count++
+        }
+        number++
+    }
+}
+fun isPrime(num: Int): Boolean {
+    if (num < 2) return false
+    for (i in 2..num / 2) {
+        if (num % i == 0) return false
+    }
+    return true
+}
+
+fun main() {
+    println("Enter how many prime numbers to print:")
+    val n = readln().toInt()
+    var count = 0
+    var number = 2
+
+    while (count < n) {
+        if (isPrime(number)) {
+            println(number)
+            count++
+        }
+        number++
+    }
+}
